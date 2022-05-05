2eaa40119ca268b4413b8acb3dc8d56b15e75be9
mihailbishkek <mihailbishkek@gmail.com>
2022-05-05 22:21:48 +0300

    Установили Октябрь.

--- modules/system/console/ComposerScript.php
+++ modules/system/console/ComposerScript.php
@@ -1,7 +1,7 @@
 <?php namespace System\Console;
 
-use Composer\Script\Event;
 use Composer\Installer\PackageEvent;
+use Composer\Script\Event;
 
 /**
  * ComposerScript is a collection of composer script logic
@@ -16,20 +16,52 @@ class ComposerScript
      */
     public static function postAutoloadDump(Event $event)
     {
-        // passthru('php artisan package:discover');
+        passthru('php artisan package:discover');
     }
 
     /**
-     * postUpdateCmd
+     * postUpdateCmd occurs after the update command has been executed, or after
+     * the install command has been executed without a lock file present.
      */
     public static function postUpdateCmd(Event $event)
     {
+        passthru('php artisan october:util set build');
+
+        passthru('php artisan october:mirror --composer');
     }
 
     /**
-     * prePackageUninstall
+     * prePackageUninstall occurs before a package is uninstalled
      */
     public static function prePackageUninstall(PackageEvent $event)
     {
+        $package = $event->getOperation()->getPackage();
+
+        if (self::isOfType($package, 'plugin')) {
+            passthru("php artisan plugin:remove ${package} --composer");
+        }
+
+        // Purge discovered package cache to prevent errors
+        if (file_exists($packagesMeta = __DIR__ . '/../../../storage/framework/packages.php')) {
+            @unlink($packagesMeta);
+        }
+    }
+
+    /**
+     * isOfType checks if a package is a plugin or theme
+     *
+     * rainlab-vanilla-theme dev-master, theme -> true
+     */
+    protected static function isOfType(string $package, string $type): bool
+    {
+        if (substr($package, -strlen('-'.$type)) === (string) '-'.$type) {
+            return true;
+        }
+
+        if (strpos($package, '-'.$type.'-') !== false) {
+            return true;
+        }
+
+        return false;
     }
 }
